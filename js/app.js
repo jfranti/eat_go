8f7b1d08b632a5b02172fc136c7956dd5d440df3
John <jfranti@gmail.com>
2016-10-02 15:44:33 -0700

    broken, working on adding and removing meals

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 // BACK END LOGIC
+var mealList = [];
 
 function Meal(title, cost)
 {
   this.title = title;
-  this.cost = parseInt(cost);
+  this.price = cost;
+  this.id = mealList.length + 1;
+  mealList.push(this);
 }
 
 var Item1 = new Meal("Chicken Parm", 12);
@@ -11,48 +14,93 @@ var Item2 = new Meal("Artichoke Dip", 5);
 var Item3 = new Meal("IPA Beer", 6);
 var Item4 = new Meal("Garden Salad", 7);
 var Item5 = new Meal("Fish & Chips", 14);
-var mealList = [Item1,Item2,Item3,Item4,Item5];
 
 var myItems = [];
 
-Meal.prototype.addToMyItems = function(){
-  myItems.push(this);
+var find = function(id) {
+  return mealList[id - 1];
+}
+
+var addToMyItems = function(id){
+  myItems.push(find(id));
 }
 
 function getTotal() {
   totalCost = 0;
   for (var i = 0; i < myItems.length; i++) {
-    totalCost += myItems[i].cost;
+    totalCost += myItems[i].price;
   }
+  return totalCost;
 }
 
 
 // FRONT END LOGIC
 
 $(document).ready(function() {
-
+  mealList.forEach(function(meal) {
+    $("#list").append("<hr><button class='btn btn-success col-md-6 mealItem' value=" + meal.id + ">" + meal.title + ", $" + meal.price + "</button>");
+  });
 
   $('body').click(function() {
     console.log("It works");
   })
 
-  // THIS FUNCTION ADVANCES BETWEEN THE INITIAL PAGES
-  $('.advance').click(function() {
-      if ($('.page1').attr('style') != "display: none;"){
-        console.log("I read the style.");
-        $('.page1').hide();
-        $('.page2').show();
-        console.log($('.page1').attr('style'));
-      } else if ($('.page2').attr('style') != "display: none;"){
-        $('.page2').hide();
-        $('.page3').show();
-      }
-  });
+  $(".mealItem").click(function() {
+    var currentId = $(this).val();
+    $(this).toggleClass("btn-danger");
 
-  $('.ticket-number-form').submit(function(event) {
-    event.preventDefault();
-    
-  })
+    for (var i = 0; i <= myItems.length; i++) {
+      if (currentId === myItems[i].id){
+        console.log("i found a match");
+        myItems.splice(i, 1);
+      } else {
+        addToMyItems(currentId);
+      }
+
+
+
+
+      // console.log("we're in the for loop");
+      // if (myItems.length == 0) {
+      //   console.log(myItems.length);
+      //   addToMyItems(currentId);
+      //   break;
+      // } else if (myItems[i].id === parseInt(currentId)) {
+      //   // console.log("you already are paying for this meal");
+      //   myItems.splice(i, 1);
+      //   break;
+      // } else {
+      //   addToMyItems(currentId);
+      //   break;
+      // }
+    }
+
+  $("#total").text(parseInt(getTotal()));
+});
+
+
+// THIS FUNCTION ADVANCES BETWEEN THE INITIAL PAGES
+$('.advance').click(function() {
+  if ($('.page1').attr('style') != "display: none;"){
+    console.log("I read the style.");
+    $('.page1').hide();
+    $('.page2').show();
+    console.log($('.page1').attr('style'));
+  } else if ($('.page2').attr('style') != "display: none;"){
+    $('.page2').hide();
+    $('.page3').show();
+  }
+});
+
+
+$('.ticket-number-form').submit(function(event) {
+  event.preventDefault();
+  if ($("#meal-id").val() === "123ABCD") {
+    $(location).attr('href', './app-demo.html');
+  } else {
+    $(".output").text("PLEASE ENTER A VALID RECEIPT NUMBER");
+  }
+})
 
 
 
