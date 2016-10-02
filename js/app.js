9a9d51ff4227e7d46288b032554a38e64a2445f0
John <jfranti@gmail.com>
2016-10-02 14:13:29 -0700

    add third page

--- js/app.js
+++ js/app.js
@@ -13,12 +13,32 @@ $(document).ready(function() {
   })
 
   $(document).keydown(function(e) {
-    // console.log(e.keyCode);
+    console.log($('.page1').attr('style'));
     if (e.keyCode = 32) {
-      $('.page1').hide();
-      $('.page2').show();
+
+      if ($('.page1').attr('style') != "display: none;"){
+        console.log("I read the style.");
+        $('.page1').hide();
+        $('.page2').show();
+      } else if ($('.page2').attr('style') != "display: none;"){
+            $('.page2').hide();
+            $('.page3').show();
+      }
+      console.log($('.page1').attr('style'));
+
+
     }
-  })
+  });
+
+  // $(document).keydown(function(e) {
+  //   if (e.keyCode = 32) {
+  //     $('.page2').hide();
+  //     $('.page3').show();
+  //   }
+  // });
+
+
+
 
 
 })
